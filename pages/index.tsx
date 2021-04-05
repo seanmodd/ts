6f4cd5cfd97fedb50c0ffa9dbc46dfa4604cf915
Sean <sean@vestaridefair.com>
2021-04-05 13:59:35 -0700

    second

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import { Heading } from '@chakra-ui/layout'
 import type { TypeA } from '../types'
 import { GoogleFonts } from 'next-google-fonts';
+
+const font = `Inter`
+const url = `https://fonts.googleapis.com/css2?family=${font}:ital,wght@0,200;0,300;0,400&display=swap`
+
+
+
+
 const Example: TypeA = {
   name: 'next',
 }
@@ -9,9 +16,12 @@ export default function Home() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <h1>
+        <GoogleFonts href={url} />
+        <Heading fontFamily={font} >Here</Heading>
+        <p style={{ fontFamily: { font } }}>{Example.name}</p>
         <p>{Example.name}</p>
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
-        <Heading fontFamily="Poppins">Here</Heading>
+        <p style={{ fontFamily: { font } }}>wow</p>
+        <p>wow</p>
       </h1>
     </div>
   )
